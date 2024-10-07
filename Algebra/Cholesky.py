f12418a84b8a8bf841cb82a6aaccc962877c05e2
mohit <tarachandanimohit9@gmail.com>
2024-10-08 00:55:42 +0530

    fixed issues

--- Algebra/Cholesky.py
+++ Algebra/Cholesky.py
@@ -1,24 +1,24 @@
+import numpy as np
 from math import sqrt
-from numpy import array
 
-def Cholesky(A):
-    L = [[0.0 for x in range(len(A))] for y in range(len(A))]
-    L = array(L)
+def cholesky_decomposition(A):
+    """
+    Perform Cholesky decomposition on a matrix A.
+    A must be a symmetric, positive-definite matrix.
+    
+    Returns the lower triangular matrix L such that A = L * L^T.
+    """
+    n = len(A)
+    L = np.zeros((n, n))  # Initialize L with zeros
 
-    for k in range(len(A)):
-        for i in range(len(A)):
-            if k is not i:
-                L[k][i] += A[k][i]
-                for j in range(i-1):
-                    L[k][i] -= L[i][j]*L[k][j]
-                if L[i][i] == 0:
-                    L[k][i] = 0
-                else:
-                    L[k][i] /= L[i][i]
-            else:
-                L[k][k] += A[k][k]
-                for j in range(k-1):
-                    L[k][k] -= pow(L[k][j], 2)
-                L[k][k] = sqrt(L[k][k])
+    for k in range(n):
+        # Calculate the diagonal element L[k][k]
+        sum_squares = sum(L[k][j] ** 2 for j in range(k))
+        L[k][k] = sqrt(A[k][k] - sum_squares)
+
+        # Calculate the off-diagonal elements L[i][k]
+        for i in range(k + 1, n):
+            sum_products = sum(L[i][j] * L[k][j] for j in range(k))
+            L[i][k] = (A[i][k] - sum_products) / L[k][k]
 
     return L
